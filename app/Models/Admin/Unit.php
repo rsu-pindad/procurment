55e32795eac18fda400d3817ee1eaa1580e886ce
rsu-pindad <sautomated01@gmail.com>
2025-06-16 16:08:34 +0700

    config unit

--- app/Models/Admin/Unit.php
+++ app/Models/Admin/Unit.php
@@ -3,11 +3,19 @@
 namespace App\Models\Admin;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Unit extends Model
+class Unit extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+
     protected $fillable = [
         'nama_unit',
         'keterangan_unit'
     ];
+
+    public function users()
+    {
+        return $this->hasMany(\App\Models\User::class);
+    }
 }
