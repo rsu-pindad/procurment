3ccf8d43a620b691f22ac9b9338f50c736bde3a9
rsu-pindad <sautomated01@gmail.com>
2025-06-17 21:56:33 +0700

    penambahan paket notyf

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,14 +1,11 @@
 import Echo from 'laravel-echo';
-
+import { Notyf } from 'notyf';
+import 'notyf/notyf.min.css';
 import Pusher from 'pusher-js';
-window.Pusher = Pusher;
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+const notyf = new Notyf();
+window.notyf = notyf;
+window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
@@ -21,8 +18,8 @@ window.Echo = new Echo({
         }
     }
 });
-
 window.Echo.private(`user.${window.userId}`)
     .listen('NotificationReceived', (e) => {
-        Livewire.dispatch('notificationReceived', e);
+        Livewire.dispatch('notificationReceived', e.message);
+        notyf.success(e.message)
     });
