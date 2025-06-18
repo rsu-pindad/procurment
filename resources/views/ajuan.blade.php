e733a7bff4e73d02bb1775bdac68ca394e44595a
rsu-pindad <sautomated01@gmail.com>
2025-06-18 12:58:16 +0700

    refactor layout

--- resources/views/ajuan.blade.php
+++ resources/views/ajuan.blade.php
@@ -8,14 +8,21 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl space-y-4">
-                    <livewire:ajuan.ajuan-form />
+                <div class="sm:flex sm:items-center">
+                    <x-section-header title="Tabel Ajuan">
+                        Berikut adalah daftar ajuan yang tersedia dengan detail informasi terkait masing-masing ajuan.
+                    </x-section-header>
+                    <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                        <livewire:ajuan.ajuan-form />
+                    </div>
                 </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <livewire:user-ajuan-table />
+                <div class="mt-4 flow-root">
+                    <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="inline-block min-w-full py-2 align-middle sm:px-4 lg:px-6">
+                            <livewire:user-ajuan-table />
+                            <livewire:ajuan.ajuan-manager />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/unit.blade.php
+++ resources/views/unit.blade.php
@@ -8,10 +8,22 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl space-y-4">
-                    <livewire:admin.unit.unit-form />
-                    <livewire:unit-table />
-                    <livewire:admin.unit.unit-manager />
+                <div class="sm:flex sm:items-center">
+                    <x-section-header title="Tabel Unit">
+                        Berikut adalah daftar unit yang tersedia lengkap dengan detail informasi terkait masing-masing
+                        unit.
+                    </x-section-header>
+                    <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                        <livewire:admin.unit.unit-form />
+                    </div>
+                </div>
+                <div class="mt-4 flow-root">
+                    <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="inline-block min-w-full py-2 align-middle sm:px-4 lg:px-6">
+                            <livewire:unit-table />
+                            <livewire:admin.unit.unit-manager />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
