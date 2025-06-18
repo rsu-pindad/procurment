ce00cd62e801d52676b3cf0ba4d26fea3952f250
rsu-pindad <sautomated01@gmail.com>
2025-06-18 13:57:24 +0700

    penambahan key

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -24,6 +24,7 @@ const notyf = new Notyf({
 window.Ably = Ably;
 window.Echo = new Echo({
     broadcaster: 'ably',
+    key: import.meta.env.VITE_ABLY_PUBLIC_KEY,
     tls: true,
     disableStats: true,
     authEndpoint: '/broadcasting/auth',
