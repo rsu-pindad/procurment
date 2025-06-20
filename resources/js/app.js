96aba6cb7b4b35f5b87116cc086fbadc42b09ad5
rsu-pindad <sautomated01@gmail.com>
2025-06-20 16:13:17 +0700

    penambahan dayjs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,10 @@
 import './bootstrap';
 
 import'./../../vendor/power-components/livewire-powergrid/dist/tailwind.css';
-import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
+import './../../vendor/power-components/livewire-powergrid/dist/powergrid';
+import dayjs from 'dayjs';
+import 'dayjs/locale/id';
+import relativeTime from 'dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
+dayjs.locale('id');
+window.dayjs = dayjs;
