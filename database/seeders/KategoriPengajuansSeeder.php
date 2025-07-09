79475bff1dcdc47152a457412920eb40ed854e3c
rsu-pindad <sautomated01@gmail.com>
2025-07-09 09:19:35 +0700

    pembaruan seeder kategori pengajuan

--- database/seeders/KategoriPengajuansSeeder.php
+++ database/seeders/KategoriPengajuansSeeder.php
@@ -10,23 +10,12 @@ class KategoriPengajuansSeeder extends Seeder
     public function run(): void
     {
         $kategori = [
-            ['nama_kategori' => 'Pengadaan Barang', 'deskripsi_kategori' => 'Kategori untuk pengadaan barang'],
-            ['nama_kategori' => 'Jasa Konsultansi', 'deskripsi_kategori' => 'Kategori untuk jasa konsultansi'],
-            ['nama_kategori' => 'Pemeliharaan', 'deskripsi_kategori' => 'Kategori untuk pemeliharaan rutin'],
-            ['nama_kategori' => 'Lain-lain', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k1', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k2', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k3', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k4', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k5', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k6', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k7', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k8', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k9', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k10', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k11', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k12', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
-            ['nama_kategori' => 'k13', 'deskripsi_kategori' => 'Kategori tambahan lainnya'],
+            ['nama_kategori' => 'Umum', 'deskripsi_kategori' => 'kategori umum pengadaan barang'],
+            ['nama_kategori' => 'Alat Kesehatan', 'deskripsi_kategori' => 'kategori alat kesehatan'],
+            ['nama_kategori' => 'Konstruksi', 'deskripsi_kategori' => 'kategori konstruksi barang'],
+            ['nama_kategori' => 'Jasa', 'deskripsi_kategori' => 'kategori jasa'],
+            ['nama_kategori' => null, 'deskripsi_kategori' => ''],
+            ['nama_kategori' => 'Investaris Kantor', 'deskripsi_kategori' => 'kategori investaris kantor'],
         ];
 
         foreach ($kategori as $item) {
