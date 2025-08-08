735ae4ba857ce76517f5ac57ad61d3c307429b8b
rsu-pindad <sautomated01@gmail.com>
2025-08-08 10:10:13 +0700

    remodel layout

--- resources/views/beranda/indexs.blade.php
+++ resources/views/beranda/indexs.blade.php
@@ -4,11 +4,10 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-6">
             @if (auth()->user()->hasRole('pengadaan'))
-                <livewire:beranda.chart-hps />
+            <livewire:beranda.chart-hps />
             @endif
             <livewire:beranda.chart-status />
             <livewire:beranda.chart-tempo />
