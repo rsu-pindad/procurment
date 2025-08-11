040581af6edcd7320b7ae730b716a412dcb488c8
rsu-pindad <sautomated01@gmail.com>
2025-08-11 12:42:48 +0700

    revisi layout

--- resources/views/components/toggle-switch.blade.php
+++ resources/views/components/toggle-switch.blade.php
@@ -1,31 +1,21 @@
 @props([
-    'id',
-    'name',
-    'type' => 'checkbox', // default ke checkbox
-    'label' => '',
-    'description' => '',
-    'checked' => false,
-    'value' => ''
+'id',
+'name',
+'type' => 'checkbox', // default ke checkbox
+'label' => '',
+'description' => '',
+'checked' => false,
+'value' => ''
 ])
 
 @php
-    $isRadio = $type === 'radio';
+$isRadio = $type === 'radio';
 @endphp
 
 <div class="flex items-center justify-between gap-3">
     <label for="{{ $id }}" class="relative inline-flex items-center cursor-pointer">
         <!-- Hidden input -->
-        <input
-            type="{{ $type }}"
-            id="{{ $id }}"
-            name="{{ $name }}"
-            value="{{ $value }}"
-            @checked($checked)
-            class="sr-only peer"
-            aria-labelledby="{{ $id }}-label"
-            aria-describedby="{{ $id }}-description"
-            {{ $attributes }}
-        />
+        <input type="{{ $type }}" id="{{ $id }}" name="{{ $name }}" value="{{ $value }}" @checked($checked) class="sr-only peer" aria-labelledby="{{ $id }}-label" aria-describedby="{{ $id }}-description" {{ $attributes }} />
 
         <!-- Background track -->
         <div class="w-11 h-6 bg-gray-200 transition-colors duration-200 ease-in-out rounded-full ring-1 ring-inset ring-gray-900/5 peer-checked:bg-indigo-600"></div>
@@ -42,10 +32,10 @@
 
     <div class="text-sm">
         @if ($label)
-            <label id="{{ $id }}-label" for="{{ $id }}" class="font-medium text-gray-900">{{ $label }}</label>
+        <label id="{{ $id }}-label" for="{{ $id }}" class="font-medium text-gray-900">{{ $label }}</label>
         @endif
         @if ($description)
-            <span id="{{ $id }}-description" class="text-gray-500">{{ $description }}</span>
+        <span id="{{ $id }}-description" class="text-gray-500">{{ $description }}</span>
         @endif
     </div>
 </div>
