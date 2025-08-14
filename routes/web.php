b90d220a276982b8259c0573387c6354384d3462
rsu-pindad <sautomated01@gmail.com>
2025-08-14 15:37:27 +0700

    pemindahan volt route ke laravel route standar

--- routes/web.php
+++ routes/web.php
@@ -20,32 +20,32 @@ Route::middleware(['guest'])->group(function () {
 
 
 Route::middleware(['auth'])->group(function () {
-    Route::view('dashboard', 'beranda.index')->name('dashboard');
-    Route::view('profile', 'profile')->name('profile');
+
+    Route::view('dashboard', 'pages.dashboard.index')->name('dashboard');
+    Route::view('profile', 'pages.profile.index')->name('profile');
 
     // role admin
     Route::group(['middleware' => ['role:admin']], function () {
-        Route::view('unit', 'unit')->name('unit');
-        Route::view('kategori', 'kategori')->name('kategori');
+        Route::view('unit', 'pages.unit.index')->name('unit');
+        Route::view('kategori', 'pages.kategori.index')->name('kategori');
     });
 
     // role admin dan pengadaan
     Route::group(['middleware' => ['role:admin|pengadaan']], function () {
-        Route::view('status-ajuan', 'status-ajuan')->name('status-ajuan');
+        Route::view('status-ajuan', 'pages.status-ajuan.index')->name('status-ajuan');
         Route::group(['prefix' => 'manajemen'], function () {
-            Route::view('user', 'manajemen.user')->name('manajemen.user');
+            Route::view('user', 'pages.manajemen.user')->name('manajemen.user');
             Volt::route('user/unit/{user}', 'user.manajemen-unit')->name('manajemen.user.unit');
             Volt::route('user/role/{user}', 'user.manajemen-role')->name('manajemen.user.role');
         });
 
-        Route::view('vendors', 'vendors')->name('vendors');
+        Route::view('vendors', 'pages.vendors.index')->name('vendors');
     });
 
     // role pengadaan dan pegawai
     Route::group(['middleware' => ['role:pengadaan|pegawai']], function () {
-        Route::view('ajuan', 'ajuan')->name('ajuan');
-        Volt::route('ajuan-detail/{ajuan}', 'ajuan.ajuan-detail')->name('ajuan.detail');
-        Volt::route('ajuan-edit/{ajuan}', 'ajuan.ajuan-edit')->name('ajuan.edit');
+        Route::view('ajuan', 'pages.ajuan.index')->name('ajuan');
+        Route::view('ajuan-detail/{ajuan}', 'pages.ajuan.detail')->name('ajuan.detail');
         Route::get('/rab/{filename}', [RabFileController::class, 'show'])
             ->where('filename', '.*')
             ->name('rab.show');
@@ -55,11 +55,10 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/analisa/{filename}', [NodinFileController::class, 'show'])
             ->where('filename', '.*')
             ->name('analisa.show');
-        // Route::view('monitor', 'monitor')->name('monitor');
     });
 
-    Route::group(['middleware' => ['role:monitoring']], function(){
-        Route::view('monitor','monitor')->name('monitor');
+    Route::group(['middleware' => ['role:monitoring']], function () {
+        Route::view('monitor', 'pages.monitor.index')->name('monitor');
     });
 });
 
